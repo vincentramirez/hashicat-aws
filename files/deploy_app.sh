4d7fc487c8cf98888e0c9e0dd6a0d696ee3f10f2
Vinnie Ramirez <vinnie.ramirez@gmail.com>
2021-02-11 09:18:54 -0700

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s Day 2 of the TFC workshop.
+  Welcome to ${PREFIX}'s This workshop is super fun.
   <!-- END -->
 
   </div>
